a498e233fa3f89851836eaad3e70d4f504527c61
unknown <liangzhicheng@MZKJ-PC-00934.meizu.com>
2016-12-21 10:50:27 +0800

    add data

--- util/mysql/Mysql.py
+++ util/mysql/Mysql.py
@@ -12,18 +12,24 @@ import MySQLdb
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
-if __name__ == '__main__':
 
-    result = open('film.txt', 'w+')
+
+def exeSql(fileName, Sql):
+    result = open(fileName, 'w+')
     conn = MySQLdb.connect(host='172.16.10.30', port=3306, user='test', passwd='meizu.com', db='MEIZU_CAL',
                            charset='utf8')
     cursor = conn.cursor()
-    cursor.execute('SELECT FTITLE,FEXT,FDESC FROM MEIZU_CAL.T_NEW_COLUMN where FPARENTID = 6;')
+    cursor.execute(Sql)
     results = cursor.fetchall()
     for row in results:
-        # print list(row)
         result.writelines('\t'.join(list(row)) + '\n')
     result.close()
     cursor.close()
     conn.commit()
     conn.close()
+
+
+if __name__ == '__main__':
+    # Sql = 'SELECT FTITLE,FEXT,FDESC FROM MEIZU_CAL.T_NEW_COLUMN where FPARENTID = 6'
+    Sql = "SELECT FTITLE,FEXT,FDESC FROM MEIZU_CAL.T_NEW_COLUMN where FPARENTID = 7 and FDESC != ''"
+    exeSql('teleplay.txt', Sql)
